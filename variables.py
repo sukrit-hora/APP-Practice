a9c7b6358a22207e1b88fb1289a8ee46128d1335
Sukrit Hora <144891617+sukrit-hora@users.noreply.github.com>
2026-07-27 09:31:14 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+x = 15
+
+class Student:
+    college = "MIT ADT"
+
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        marks = 80
+        print(x)
+        print(self.college)
+        print(self.name)
+        print(marks)
+
+s = Student("Sukrit")
+s.show()
